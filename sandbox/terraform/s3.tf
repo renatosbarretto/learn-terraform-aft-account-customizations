9f4c7c429ef18314c9bbc2ab087c9fda59ad097c
renatobarretto <renatobarretto@hotmail.com>
2025-05-03 18:49:08 -0300

    ajuste

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,7 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+resource "aws_iam_role" "aft_states" {
+  name = "aft-account-provisioning-customizations-role"
+  # ... (outros argumentos necessários, como assume_role_policy)
+}
--- sandbox/terraform/ssm.tf
+++ sandbox/terraform/ssm.tf
@@ -1,6 +1,5 @@
-resource "aws_ssm_parameter "foo" {
-    name = "/account_id"
-    type = "string""
-    value = "${data.aws_caller_identity.current.account_id}"
+resource "aws_ssm_parameter" "foo" {
+  name  = "/accID"
+  type  = "String"
+  value = data.aws_caller_identity.current.account_id
 }
-    
