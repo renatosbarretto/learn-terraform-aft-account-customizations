b38f628943d975ca539ac2bd7a47373f8ad95315
renatobarretto <renatobarretto@hotmail.com>
2025-05-03 19:26:20 -0300

    ajuste

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,11 +4,6 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
 }
 
-resource "aws_s3_bucket_acl" "sandbox_bucket_acl" {
-  bucket = aws_s3_bucket.sandbox_bucket.id
-  acl    = "private"
-}
-
 resource "aws_iam_role" "aft_states" {
   name = "aft-account-provisioning-customizations-role"
   assume_role_policy = jsonencode({
