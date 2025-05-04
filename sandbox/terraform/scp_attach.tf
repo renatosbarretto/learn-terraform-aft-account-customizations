37ebbeeaddfb57ecb443a0a2fb86980a7f55f2b2
renatobarretto <renatobarretto@hotmail.com>
2025-05-04 14:33:36 -0300

    scp

--- sandbox/terraform/scp_attach.tf
+++ sandbox/terraform/scp_attach.tf
@@ -1,4 +1,4 @@
-resource "aws_organizations_policy_attachment" "attach_scp_sandbox" {
-  policy_id = "p-1d0zt48j" # Apenas o ID da política, não o ARN completo
-  target_id = "350910063325" # ID da conta sandbox
+resource "aws_organizations_policy_attachment" "attach_scp_to_sandbox" {
+  policy_id = "p-1d0zt48j"                          # Apenas o ID da SCP
+  target_id = var.account_id                        # Injetado automaticamente
 }
