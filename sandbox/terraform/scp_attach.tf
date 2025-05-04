1cb03f99f0c19f33e25890668106e8e149834f93
renatobarretto <renatobarretto@hotmail.com>
2025-05-04 15:48:31 -0300

    scp

--- sandbox/terraform/scp_attach.tf
+++ sandbox/terraform/scp_attach.tf
@@ -1,3 +1,8 @@
+  provider "aws" {
+    alias  = "org"
+    region = "us-east-1"
+  }
+
   resource "aws_organizations_policy_attachment" "attach_scp_to_sandbox" {
     provider  = aws.org
     policy_id = "p-1d0zt48j"
